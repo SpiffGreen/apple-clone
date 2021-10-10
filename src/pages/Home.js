63cc56a5d9f30f67ed8f1effaf3c6f0d959f5909
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2021-10-10 22:55:44 +0100

    Fix: Fixed the styles and content of the aside section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import React from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
+import ipad from "../assets/images/logo-tile__ec23lf7d072a_large.png";
+
 function Home() {
 
   return (
@@ -26,25 +28,26 @@ function Home() {
         </section>
       </main>
       <aside>
-        <div>
-          <h1 className="bigText">iPad Pro</h1>
-          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
-          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        <div className="imac">
+          <h1>iMac</h1>
+          <h3>Say hello.</h3>
+          <a href="#learnmore" className="smallerText">Learn more &rang;</a>
         </div>
-        <div>
-          <h1 className="bigText">iPad Pro</h1>
-          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
-          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        <div className="airtag">
+          <h1>AirTag</h1>
+          <h3>Lose your knack for loosing things.</h3>
+          <a href="#learnmore" className="smallerText">Learn more &rang;</a>
         </div>
-        <div>
-          <h1 className="bigText">iPad Pro</h1>
-          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
-          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        <div className="airpod">
+          <h1>AirPods Max</h1>
+          {/* <h3 className="midText">Supercharged by the Apple M1 chip.</h3> */}
+          <a href="#learnmore" className="smallerText">Learn more &rang;</a>
         </div>
-        <div>
-          <h1 className="bigText">iPad Pro</h1>
-          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
-          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        <div className="ipad">
+          {/* <h1>iPad</h1> */}
+          <img src={ipad} alt="ipad" width={100} />
+          <h3>Delightfully capable.<br />Surprisingly affordable.</h3>
+          <a href="#learnmore" className="smallerText">Learn more &rang;</a>
         </div>
       </aside>
       <Footer />
