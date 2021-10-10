714c6ac7fc88a66f1a3184ee00d6ee1a002032d3
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2021-10-10 17:12:54 +0100

    Feature: Added aside section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,15 +10,43 @@ function Home() {
       <Nav />
       <main>
         <section className="iphone12 banner">
-          iphone 12
+          <h1 className="bigText">iPhone 12</h1>
+          <h3 className="midText">Blast past fast.</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
         </section>
         <section className="iphone12Pro">
-          iphone 12 Pro
+          <h1 className="bigText">iPhone 12 Pro</h1>
+          <h3 className="midText">It's a leap year</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
         </section>
         <section className="ipadpro">
-          ipad pro
+          <h1 className="bigText">iPad Pro</h1>
+          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
         </section>
       </main>
+      <aside>
+        <div>
+          <h1 className="bigText">iPad Pro</h1>
+          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        </div>
+        <div>
+          <h1 className="bigText">iPad Pro</h1>
+          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        </div>
+        <div>
+          <h1 className="bigText">iPad Pro</h1>
+          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        </div>
+        <div>
+          <h1 className="bigText">iPad Pro</h1>
+          <h3 className="midText">Supercharged by the Apple M1 chip.</h3>
+          <a href="#learnmore" className="smallText">Learn more &rang;</a>
+        </div>
+      </aside>
       <Footer />
     </>
   );
